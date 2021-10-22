1e8fc6048580bb01a0895782ccb983282a8257c3
Austin Moore <austinchristianmoore@gmail.com>
2021-10-22 13:55:41 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Tom Cruise"
+is_active = true
+famous_person = "Daniel Craig"
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+animals = ["zebra", "otter", "cat"]
+animals.shift() #removes first element of array
+puts famous_person.size #counts characters in string including space & prints
