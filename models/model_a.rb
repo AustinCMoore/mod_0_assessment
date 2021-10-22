ffa7f17f6acde9989ae20c214133c5447758e524
Austin Moore <austinchristianmoore@gmail.com>
2021-10-22 14:10:17 -0400

    Add Sky Harbor Data

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+phoenix_sky_harbor = {
+  "address" => "3400 E Sky Harbor Blvd",
+  "slogan" => "America's Friendliest Airport!",
+  "terminal_quantity" => 2,
+  "baggage_claim" => 8,
+  "starbucks" => true,
+  "chipotle" => false,
+  "airline_companies" => ["Southwest", "Delta", "United"],
+  "transportation_types" => ["Light Rail", "Uber", "Lyft", "Taxi", "Bus"],
+}
