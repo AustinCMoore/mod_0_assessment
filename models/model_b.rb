cd8f195ac7ab33e5bb3d700352640e2434d01c0a
Austin Moore <austinchristianmoore@gmail.com>
2021-10-22 14:19:52 -0400

    Add My Vehicle Data

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+my_car = {
+  "make" => "Dodge",
+  "model" => "Charger",
+  "year" => 2010,
+  "engine_liters" => 2.7,
+  "v6_engine" => true,
+  "v8_engine" => false,
+  "defects" => ["Dents", "Scratch", "Spark Plugs"],
+  "previous_owners" => ["Hertz", "Austin M."]
+}
